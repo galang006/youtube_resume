1e9e98f596b014d36fe7e32ac5c0acb39db891a6
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2025-08-06 15:03:14 +0700

    update

---
+++ llm_api.py
@@ -0,0 +1,17 @@
+import warnings
+
+warnings.filterwarnings("ignore", message="Field name .* shadows an attribute in parent .*")
+
+from google import genai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words",
+)
+print(response.text)
