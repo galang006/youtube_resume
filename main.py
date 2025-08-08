d5ab6e49493024025911c7a4c503be7f4ba0cb07
Galang Satriamahesa Putra Dewa <galangsatriamahesa.pd@gmail.com>
2025-08-08 14:28:09 +0700

    update

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 from gemini_api.client import generate_content
 from youtube_utils.transcriber import get_transcript
+import os
+import re
+
+def clean_filename(filename):
+    # Hapus karakter ilegal di Windows: \ / : * ? " < > |
+    return re.sub(r'[\\/:*?"<>|]', '_', filename)
 
 if __name__ == "__main__":
-    video_url = 'https://www.youtube.com/watch?v=0kLvVINbCtk'
-    transcript = get_transcript(video_url)
+    output_dir = "./resume"
+    os.makedirs(output_dir, exist_ok=True)
 
+    video_url = input("Enter YouTube video URL: ")
+
+    print(f"Get Transcript for video...")
+    title, transcript = get_transcript(video_url)
+    
+    if not transcript:
+        print("Transcript not available.")
+        exit()
+
+    print(f"Generating summary for video: {title}")
     response = generate_content(transcript)
 
-    print(response)
+    title_clean = clean_filename(title)
+    output_file = f"{output_dir}/{title_clean}.txt"
 
+    with open(output_file, "w", encoding="utf-8") as f:
+        f.write(response)
+
+    print(f"Summary saved to {output_file}")
     
--- youtube_utils/transcriber.py
+++ youtube_utils/transcriber.py
@@ -1,11 +1,23 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 
+import yt_dlp
+
+def get_video_title(video_url):
+    ydl_opts = {
+        'quiet': True,
+        'skip_download': True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(video_url, download=False)
+        return info.get('title', 'No Title Found')
+
 def get_transcript(video_url):
     """
     Fetch the transcript for a given YouTube video ID.
     :param video_id: The ID of the YouTube video.
     :return: The transcript of the video.
     """
+    title = get_video_title(video_url)
     video_id = video_url.split('v=')[-1] if 'v=' in video_url else video_url.split('/')[-1]
 
     transcript = {}
@@ -16,7 +28,7 @@ def get_transcript(video_url):
         for snippet in fetched_transcript:
             transcript[snippet.start] = snippet.text
 
-        return transcript
+        return title, transcript
     
     except Exception as e:
         print("Error:", e)
